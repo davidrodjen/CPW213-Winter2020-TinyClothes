983fbcc4e3d701b07c9619a669012f1cc080834b
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-01-21 10:34:32 -0800

    Added DbContext class

---
+++ TinyClothes/Data/StoreContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TinyClothes.Models;
+
+namespace TinyClothes.Data
+{
+    public class StoreContext : DbContext //Right click solution > click manage nuget, install microsoft.entity.frameworkcore
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+            
+        }
+        /// <summary>
+        /// Add a DbSet for each Entity that needs
+        /// to be tracked by the DB
+        /// https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-3.1
+        /// </summary>
+        public DbSet<Clothing> Clothing { get; set; } //property names can be the same as class names in C#
+    }
+}
---
+++ TinyClothes/Models/Clothing.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Models
+{   
+    /// <summary>
+    /// Represents a single clothing item
+    /// </summary>
+    public class Clothing
+    {
+        /// <summary>
+        /// The unique id for the clothing item
+        /// </summary>
+        [Key] // Set as PK(Primary Key)
+        public int ItemId { get; set; }
+
+        /// <summary>
+        /// The Size of the lcothing (Small, medium, large)
+        /// </summary>
+        public string Size { get; set; }
+
+        /// <summary>
+        /// Type of clothing, shirt, pants, etc
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// The color of the clothing item
+        /// </summary>
+        public string Color { get; set; }
+
+        /// <summary>
+        /// Retail price of the item
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Display the title of the clothing
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Description of item
+        /// </summary>
+        public string Description { get; set; }
+
+    }
+}
