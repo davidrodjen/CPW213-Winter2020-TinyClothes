49234e946b5b1d46b33107f3f6e7433d3ab9667d
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-01-23 11:00:49 -0800

    Added Clothes controller and prepared for an Insert Query

---
+++ TinyClothes/Controllers/ClothesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TinyClothes.Controllers
+{
+    public class ClothesController : Controller
+    {
+        [HttpGet]
+        public IActionResult ShowAll()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ TinyClothes/Data/ClothingDb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TinyClothes.Models;
+
+namespace TinyClothes.Data
+{
+
+    /// <summary>
+    /// Contains DB helper methods
+    /// for <see cref="Modles.Clothing""/>
+    /// </summary>
+    public static class ClothingDb
+    {
+        public static List<Clothing> GetAllClothing()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Adds a clothing object ot the database
+        /// Returns the object with the Id populated
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static Clothing Add(Clothing c, StoreContext context) 
+        {
+            context.Add(c); //prepares INSERT query
+            context.SaveChanges(); //execute INSERT query
+
+            return c;
+        }
+    }
+}
