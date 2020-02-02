db99e042a9380433415c102b9caeb891620829bf
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-02-02 10:40:11 -0800

    Added clothing controller and Clothing DB

---
+++ TinyClothes/Controllers/ClothesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TinyClothes.Data;
+
+namespace TinyClothes.Controllers
+{
+    public class ClothesController : Controller
+    {
+        private readonly StoreContext _context; //readonly, only constructor can modify
+
+        public ClothesController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
