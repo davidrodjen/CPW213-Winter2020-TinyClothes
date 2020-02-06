55186b7319fce6134104e270b7e542e201c32a1c
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-02-06 09:55:57 -0800

    Generated basic edit clothing page

--- TinyClothes/Controllers/ClothesController.cs
+++ TinyClothes/Controllers/ClothesController.cs
@@ -63,5 +63,14 @@ namespace TinyClothes.Controllers
             //Return same view with validation messages
             return View(c);
         }
+
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            Clothing c =
+                await ClothingDb.GetClothingById(id, _context); //context is a field in the controller, hence _context
+
+            return View(c);
+        }
     }
 }
