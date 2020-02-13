1552522d9027d546f855ebb466be95d0d806cdef
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-02-13 09:38:57 -0800

    Created Register Page

--- TinyClothes/Models/Account.cs
+++ TinyClothes/Models/Account.cs
@@ -43,4 +43,35 @@ namespace TinyClothes.Models
         public DateTime? DateOfBirth { get; set; }
 
     }
+
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(60)]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Compare confirm password to password
+        /// </summary>
+        [Required]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
 }
