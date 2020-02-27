593cff61a54e88bfc38ae6a257b7acdf758e6142
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-02-27 07:27:03 -0800

    Added shopping cart helper

---
+++ TinyClothes/Models/CartHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Models
+{
+    /// <summary>
+    /// Helper Class to manage users
+    /// shopping cart data using cookies
+    /// </summary>
+    public static class CartHelper
+    {
+        public static void Add(Clothing c, IHttpContextAccessor http)
+        {
+            //convert object to text
+            string data = JsonConvert.SerializeObject(c);
+
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddDays(14),
+                IsEssential = true,
+                Secure = true
+            };
+
+            http.HttpContext.Response.Cookies.Append("CartCookie", data);
+
+            //CookieOptions options = new CookieOptions();
+            //options.Expires
+        }
+
+        public static int GetCount(IHttpContextAccessor http)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<Clothing> GetAllClothes(IHttpContextAccessor http)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
