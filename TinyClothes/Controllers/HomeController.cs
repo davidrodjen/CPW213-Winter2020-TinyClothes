8f4b884aefe1af725bd85a19ddd2527f81e5891f
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-03-10 14:39:24 -0700

    Added a custom 404 page

--- TinyClothes/Controllers/HomeController.cs
+++ TinyClothes/Controllers/HomeController.cs
@@ -33,5 +33,15 @@ namespace TinyClothes.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult CustomError(string code)
+        {
+            if(code == "404")
+            {
+                ViewData["ErrorMsg"] = "Not found";
+            }
+
+            return View();
+        }
     }
 }
